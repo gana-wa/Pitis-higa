350d29ad3c7d1eb0945c2194d075f9a70513b6cf
ganawa <ghana.afani@gmail.com>
2020-09-25 15:13:36 +0700

    [add] index

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const cors = require('cors');
+
+const indexRouter = require('./src/routes/index');
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+   console.log(`Server is running at port ${process.env.PORT}`);
+});
+
+app.use(express.static("public"));
+app.use(bodyParser.json()); //json
+app.use(bodyParser.urlencoded({ extended: false })) //x-www-form-urlencoded
+app.use(logger("dev"));
+app.use(cors());
+
+app.use(indexRouter);
